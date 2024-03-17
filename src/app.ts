8f997e2e1e5aeff8eaeb8c7565dd78936333d0c3
Kimmi17 <dangthikimthuybl@gmail.com>
2024-03-17 17:44:11 +0200

    Create categoriesRouter

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express, { Request, Response } from "express";
 
 import productsRouter from "./routers/productsRouter";
 import usersRouter from "./routers/usersRouter";
+import categoriesRouter from "./routers/categoriesRouter";
 
 const PORT = 8080;
 
 const app = express();
 app.use(express.json());
-
 
 app.get("/", (request: Request, response: Response) => {
   response.status(200).json({ message: "Hello world!" });
@@ -15,6 +15,7 @@ app.get("/", (request: Request, response: Response) => {
 
 app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/categories", categoriesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
