660ea455f0ea159b44343d6656a4a729d1939509
Kimmi17 <dangthikimthuybl@gmail.com>
2024-03-20 10:04:50 +0200

    Add updateCategory and deleteCategoryById to category controller file

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,9 +1,7 @@
-// ANDREA
-
 import express, { Request, Response } from "express";
 
 import categoriesService from "../services/categories";
-import Category from "../model/Category";
+import Category, { CategoryDocument } from "../model/Category";
 
 export async function getAllCategories(_: Request, response: Response) {
   const categories = await categoriesService.getAllCategories();
@@ -21,4 +19,21 @@ export async function getCategoryById(request: Request, response: Response) {
     request.params.categoryId
   );
   response.status(201).json(foundCategory);
+}
+
+export async function updateCategory(request: Request, response: Response) {
+  const categoryId = request.params.categoryId;
+  const newInformation = request.body as Partial<CategoryDocument>;
+  const updatedCategory = await categoriesService.updateCategory(
+    categoryId,
+    newInformation
+  );
+  response.status(200).json(updatedCategory);
+}
+
+export async function deleteCategoryById(request: Request, response: Response) {
+  const foundCategory = await categoriesService.deleteCategoryById(
+    request.params.categoryId
+  );
+  response.status(204).json();
 }
