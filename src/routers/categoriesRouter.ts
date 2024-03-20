e986b620f5f41461bfa1adbbecf8e88b099650d4
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2024-03-20 19:38:28 +0200

    implementing admin checking function, checking admin role for createProduct and createCategory function

--- src/routers/categoriesRouter.ts
+++ src/routers/categoriesRouter.ts
@@ -7,12 +7,13 @@ import {
   updateCategory,
   deleteCategoryById,
 } from "../controllers/categories";
+import adminCheck from "../middlewares/adminCheck";
 
 const router = express.Router();
 
 router.get("/", getAllCategories);
 router.get("/:categoryId", getCategoryById);
-router.post("/", createCategory);
+router.post("/", adminCheck, createCategory);
 router.put("/:categoryId", updateCategory);
 router.delete("/:categoryId", deleteCategoryById);
 
