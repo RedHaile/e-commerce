274890d6e67cc8054702ea8d9198065c916b4569
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2024-03-28 16:56:22 +0200

    implementing admin checking role function in middleware

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { getAllProducts, createProduct, getProduct, updateProduct, deleteProduct, getCategoryProducts } from "../controllers/products";
+import verifyJWT from "../middlewares/verifyJWT";
 import adminCheck from "../middlewares/adminCheck";
 
 const router = express.Router();
@@ -13,15 +14,15 @@ router.get("/", getAllProducts)
 router.get("/category/:categoryId", getCategoryProducts)
 
 // CREATE A PRODUCT
-router.post("/", adminCheck, createProduct);
+router.post("/", verifyJWT, adminCheck, createProduct);
 
 // GET A PRODUCT
 router.get("/:productId", getProduct);
 
 // UPDATE A PRODUCT
-router.put("/:productId", updateProduct);
+router.put("/:productId", verifyJWT, adminCheck, updateProduct);
 
 // DELETE A PRODUCT
-router.delete("/:productId", deleteProduct);
+router.delete("/:productId", verifyJWT, adminCheck, deleteProduct);
 
 export default router;
--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -4,6 +4,7 @@ import express from "express";
 
 import { createUser, deleteUser, loginUser, getAllUsers, updateUser } from "../controllers/users";
 import verifyJWT from "../middlewares/verifyJWT";
+import adminCheck from "../middlewares/adminCheck";
 
 const router = express.Router();
 
