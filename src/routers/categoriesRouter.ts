fef1d9ea52d85f244a3a6c3fbab97a399400c190
RedHaile <sredeath@gmail.com>
2024-04-17 16:59:56 +0300

    minor change

--- src/routers/categoriesRouter.ts
+++ src/routers/categoriesRouter.ts
@@ -12,21 +12,16 @@ import adminCheck from "../middlewares/adminCheck";
 
 const router = express.Router();
 
-// GET ALL CATEGORIES
 router.get("/", getAllCategories);
 
-// GET CATEGORY BY ID
 router.get("/:categoryId", getCategoryById);
 
-// CREATE A CATEGORY
 // router.post("/", verifyJWT, adminCheck, createCategory);
 router.post("/", createCategory);
 
-// UPDATE A CATEGORY
 // router.put("/:categoryId", verifyJWT, adminCheck, updateCategory);
 router.put("/:categoryId", updateCategory);
 
-// DELETE A CATEGORY
 // router.delete("/:categoryId", verifyJWT, adminCheck, deleteCategoryById);
 router.delete("/:categoryId", deleteCategoryById);
 
